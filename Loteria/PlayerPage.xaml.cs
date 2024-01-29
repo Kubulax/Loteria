ac5c024ad83032fe4a63c159f78fe2b2c307de9b
Kubulax <Kubulax25@gmail.com>
2024-01-29 09:37:21 +0100

    Added all necessary views (front-end)

---
+++ Loteria/PlayerPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loteria
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerPage : ContentPage
+    {
+        public PlayerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
